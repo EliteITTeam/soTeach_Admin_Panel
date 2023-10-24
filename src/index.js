7e20c03e8bb8ebb1c0befcabbacc5c81f65e76a0
Rachel Allen <142238255+Rachel-Allen1@users.noreply.github.com>
2023-10-24 23:32:34 +0500

    bulild fixed

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from "react";
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.scss";
 import { Provider } from "react-redux";
 import store from "./store";
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <Provider store={store}>
-    <App />
+      <App />
     </Provider>
-   
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
